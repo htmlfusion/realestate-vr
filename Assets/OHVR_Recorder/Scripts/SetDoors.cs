b5e2af6eea7c08bd2686a1fa2887aad15ab1cb75
Philippe Lewicki <philippe@ofuz.org>
2015-12-11 09:08:08 -0800

    Manually record

---
+++ Assets/OHVR_Recorder/Scripts/SetDoors.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class SetDoors : MonoBehaviour {
+	public GameObject cursor;
+
+	public void RecordDoorPosition() {
+		Debug.Log (cursor.transform.position);
+	}
+}
---
+++ Assets/OHVR_Recorder/Scripts/ViewerMotor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewerMotor : MonoBehaviour {
+		
+	public GameObject Cursor; // Google Cardboard SDK: Cursor / GazePointer from CardboardMain Prefab
+	private Vector3 goal;
+	
+	void Start() {
+		this.goal = new Vector3(0f, 0f, 0f);
+	}
+	
+	//Set navigation destination to the position of the cursor
+	//Ex. Call this from an event trigger on the floor object
+	public void SetDestinationToCursor() {
+		this.goal = Cursor.transform.position;
+		Debug.Log (this.goal);
+	}
+}
